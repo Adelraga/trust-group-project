5b0b0ebb2af9e64284079c1c2083cca2baccb054
Adelraga <ar038122@gmail.com>
2024-06-17 18:24:42 +0300

    - add url launcher to lanuch any url  in services page

--- lib/features/our-services/presentation/widgets/contact_section.dart
+++ lib/features/our-services/presentation/widgets/contact_section.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:trust_group_project/cors/Themeing/colors.dart';
 
 import '../../../../cors/Helpers/spacing.dart';
 import '../../../../cors/Themeing/styles.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 class ContactSection extends StatelessWidget {
   const ContactSection({super.key});
@@ -20,40 +24,87 @@ class ContactSection extends StatelessWidget {
             style: TextStyles.font20BlackBold,
           ),
           verticalSpace(10),
-          Container(
-            height: 60,
-            width: 380,
-            decoration: BoxDecoration(
-              color: ColorsManager.secondaryColor,
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Image.asset("assets/images/websiteicon.png"),
-                Text(
-                  "https://trust-group-ry.com/",
-                  style: TextStyles.font20BlackBold,
-                ),
-              ],
+          GestureDetector(
+            onTap:() async{
+                 Uri url = Uri.parse("https://trust-group-ry.com/");
+                  if (!await launchUrl(url)) {
+                      throw Exception('Could not launch $url');    
+                    }
+              },
+            child: Container(
+              height: 60,
+              width: 380,
+              decoration: BoxDecoration(
+                color: ColorsManager.secondaryColor,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset("assets/images/websiteicon.png"),
+                  Text(
+                    "https://trust-group-ry.com/",
+                    style: TextStyles.font20BlackBold,
+                  ),
+                ],
+              ),
             ),
           ),
+
           verticalSpace(29),
+
           Text(
             ": للتواصل معانا ",
             style: TextStyles.font20BlackBold,
           ),
+
           verticalSpace(10),
+
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/images/facebook.png"),
+              GestureDetector(
+                onTap:() async{
+                 Uri url = Uri.parse("https://www.facebook.com/profile.php?id=100095142773499&mibextid=LQQJ4d");
+                  if (!await launchUrl(url)) {
+                      throw Exception('Could not launch $url');    
+                    }
+              },
+                child: Image.asset("assets/images/facebook.png")
+                ),
               horizontalSpace(8),
-              Image.asset("assets/images/telegram.png"),
+
+              GestureDetector(
+                onTap:() async{
+                 Uri url = Uri.parse("https://t.me/+vINzxThizaE1ZjNi");
+                  if (!await launchUrl(url)) {
+                      throw Exception('Could not launch $url');    
+                    }
+              },
+                child: Image.asset("assets/images/telegram.png")
+                ),
               horizontalSpace(8),
-              Image.asset("assets/images/whatapp.png"),
+
+              GestureDetector(
+                onTap:() async{
+                 Uri url = Uri.parse("https://wa.me/message/SSXROVXDPJSKC1?src=qr");
+                  if (!await launchUrl(url)) {
+                      throw Exception('Could not launch $url');    
+                    }
+              },
+                child: Image.asset("assets/images/whatapp.png")
+                ),
+
               horizontalSpace(8),
-              Image.asset("assets/images/youtube.png"),
+              GestureDetector(
+                  onTap:() async{
+                 Uri url = Uri.parse("https://www.youtube.com/@abdallaalkholy7569");
+                  if (!await launchUrl(url)) {
+                      throw Exception('Could not launch $url');    
+                    }
+              },
+                child: Image.asset("assets/images/youtube.png")
+                ),
             ],
           )
         ],
