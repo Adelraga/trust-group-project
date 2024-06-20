d43263e7e6bb5b53c2e21798cbf0c4ea6591d6f3
Adelraga <ar038122@gmail.com>
2024-06-20 17:29:10 +0300

    make rating screen

--- lib/cors/Routing/routes.dart
+++ lib/cors/Routing/routes.dart
@@ -7,6 +7,7 @@ class Routes {
    static const String KarkastanView = '/KarkastanView';
    static const String RussainView = '/RussainView';
    static const String LearnWithUsView = '/LearnWithUsView';
+   static const String RatingView = '/RatingView';
 
   // static const String profileScreen = '/profileScreen';
 }
